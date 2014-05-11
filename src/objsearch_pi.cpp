5f32ba95877a70a3175dcbdd5c04d30cb0ad55c9
Pavel Kalian <pavel@kalian.cz>
2014-05-11 14:39:51 -0500

    Fix the too many results query

--- src/objsearch_pi.cpp
+++ src/objsearch_pi.cpp
@@ -404,12 +404,12 @@ void objsearch_pi::FindObjects( const wxString& feature_filter, const wxString& 
     wxSQLite3ResultSet set = SelectFromDB( m_db, wxString::Format( wxT("SELECT COUNT(*) FROM object o LEFT JOIN feature f ON (o.feature_id = f.id) WHERE featurename LIKE '%%%s%%' AND objname LIKE '%%%s%%'"), feature_filter.c_str(), safe_value.c_str() ) );
     int objects_found = set.GetInt(0);
     set.Finalize();
-    int show = wxID_YES;
+    int show = wxYES;
     if ( objects_found > 1000 )
     {
         show = wxMessageBox( wxString::Format( _("Your search resulted in %i objects found. This is a lot, do you really want to show all of them?"), objects_found ), _("Too many objects found"), wxYES_NO | wxCENTER );
     }
-    if ( show == wxID_YES )
+    if ( show == wxYES )
     {
         set = SelectFromDB( m_db, wxString::Format( wxT("SELECT f.featurename, o.objname, o.lat, o.lon FROM object o LEFT JOIN feature f ON (o.feature_id = f.id) WHERE featurename LIKE '%%%s%%' AND objname LIKE '%%%s%%'"), feature_filter.c_str(), safe_value.c_str() ) );
         while (set.NextRow())
