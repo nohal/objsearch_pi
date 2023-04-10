052d93b857aee8a5644ffd26a54708ea87947f74
Pavel Kalian <pavel@kalian.cz>
2023-04-10 12:15:30 -0300

    Bump version

--- Plugin.cmake
+++ Plugin.cmake
@@ -32,7 +32,7 @@ set(OCPN_RELEASE_REPO
 # -------  Plugin setup --------
 #
 set(PKG_NAME objsearch_pi)
-set(PKG_VERSION "0.22.0")
+set(PKG_VERSION "0.24.0")
 set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
 
 set(DISPLAY_NAME objsearch)    # Dialogs, installer artifacts, ...
