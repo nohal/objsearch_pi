a9bed18a14e0bfc2ccae6cf01c988adf59dab932
Pavel Kalian <pavel@kalian.cz>
2023-04-22 11:38:02 -0300

    Prepare for 0.26.1 and enable Buster builds

--- Plugin.cmake
+++ Plugin.cmake
@@ -25,7 +25,7 @@ set(OCPN_RELEASE_REPO
 # -------  Plugin setup --------
 #
 set(PKG_NAME objsearch_pi)
-set(PKG_VERSION "0.26.0")
+set(PKG_VERSION "0.26.1")
 set(PKG_PRERELEASE "") # Empty, or a tag like 'beta'
 
 set(DISPLAY_NAME objsearch) # Dialogs, installer artifacts, ...
